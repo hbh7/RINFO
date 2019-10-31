58a914a5eabe1549c375c24b69d509ed47e08f69
Gio <tremag@rpi.edu>
2019-10-31 15:40:31 -0400

    started to make footer look nice
    I still need to fix the margin issue, as well as create/style a link for the site map
    I'm not sure if we are going to have a site map, but the footer was looking a little empty (it can always be removed later)
    Ok I have no idea what pushing this will do
    I accidentally committed to master instead of the new branch I created for the footer stuff, so I undid the commit, stashed it in master, then popped it in the new branch
    Hopefully this doesn't break anything

--- resources/templates/footer.php
+++ resources/templates/footer.php
@@ -1,4 +1,5 @@
-<footer>
-    <p> Made by Team Tokabo </p>
-    <img id="logo" src="/resources/images/logo.png" width=50px height=50px alt="Team Tokabo Logo">
+<footer id="global_footer">
+    <span> Made by Team Tokabo </span>
+    <span> Web Systems and Development Semester Project </span>
+    <span> Site Map </span>
 </footer>
