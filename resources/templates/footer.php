c7b1c1e875d8612161ee441e26b15cc8ea72cc31
Gio <tremag@rpi.edu>
2019-10-31 17:45:41 -0400

    footer is done for now
    Until I know what else is going there, I don't think there is any more work to be done with the footer
    Unless someone wants to add some CSS to make it look nicer

--- resources/templates/footer.php
+++ resources/templates/footer.php
@@ -1,5 +1,7 @@
-<footer id="global_footer">
-    <span> Made by Team Tokabo </span>
-    <span> Web Systems and Development Semester Project </span>
-    <span> Site Map </span>
-</footer>
+<div id="wrap_footer">
+    <footer id="global_footer">
+        <span> Made by Team Tokabo </span>
+        <span> Web Systems and Development Semester Project </span>
+        <span><a href="#"> Go To Site Map </a></span>
+    </footer>
+</div>
