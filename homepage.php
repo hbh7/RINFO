28c7d84c2f4fff187586109fe51f947aa2006240
ZhaoxiSun19991225 <47791231+ZhaoxiSun19991225@users.noreply.github.com>
2019-10-31 17:05:34 -0400

    homepage
    contain buttons got to search and login page and a place to show the current activities.

---
+++ homepage.php
@@ -0,0 +1,25 @@
+<html>
+	<head>
+        <?php include('resources/templates/head.php'); ?>
+        <title> RINFO</title>
+    </head>
+	
+    <body>
+		<img id="logo" src="/resources/images/logo.png" width=50px height=50px alt="Logo">
+		<h1 class="group_name"> Welcome to RINFO </h1>
+		<button id="search" onclick="window.location='search.php';"> Search </button>
+		<button id="login" onclick="window.location='login.php';">Log in</button>
+		
+		<div id="content">
+			<h2>Current Activity</h2>
+			<div class="content">
+				<ul>
+                    <li>....shirleyismygirly....</li>
+                    <li>....______:)______....</li>
+                </ul>
+			</div>
+		</div>
+		
+		<?php include('resources/templates/footer.php'); ?>
+	</body>
+</html>
