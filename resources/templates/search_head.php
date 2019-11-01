99be4b2f2256e8686b4436eb2e240509887fbeb6
ZhaoxiSun19991225 <47791231+ZhaoxiSun19991225@users.noreply.github.com>
2019-11-01 02:46:07 -0400

    Create search_head.php

---
+++ resources/templates/search_head.php
@@ -0,0 +1,5 @@
+<!-- Load scripts, styles, etc for search page here -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<link rel="stylesheet" type="text/css" href="/resources/styles/search_styles.css">
+<link rel="stylesheet" type="text/css" href="/resources/styles/styles-footer.css">
+<link rel="stylesheet" type="text/css" href="/resources/styles/styles-header.css">
