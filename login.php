3696daad4abd11520f33c580c61a27aa1b79ff94
Lanya Xiang <xianlany2@gmail.com>
2019-10-31 22:54:15 -0400

    Added more CSS to login page
    added a background and centered the items

--- login.php
+++ login.php
@@ -4,22 +4,22 @@
         <title> RINFO Login </title>
     </head>
     <body>
-        <?php include('resources/templates/header.php'); ?>
 
         <div id="login_content">
             <div id="info_panel">
-                <h2>Subscribe to groups</h2>
-                <h2>Browse personalized event feeds</h2>
-                <h2>Discover new interests</h2>
-                <h2>Get to know your campus better</h2>
+                <div class="tips">
+                    <h2>Subscribe to groups</h2>
+                    <h2>Browse personalized event feeds</h2>
+                    <h2>Discover new interests</h2>
+                    <h2>Get to know your campus better</h2>
+                </div>
             </div>
             <div id="login_panel">
                 <div id="login_box">
-                    Username
-                    <input type = "text" id = "username" value = "Username">
+                    <p>Username <input type = "text" id = "username" value = "Username"></p>
                     <br>
-                    Password 
-                    <input type = "text" id = "password" value = "Password">
+                    <p>Password   <input type = "text" id = "password" value = "Password"></p>
+                    <p>Forgot Password</p>
                 </div>
             </div>
         </div>
