56217e1a6beb975a326f58b70382a8c1adf4febd
Gio <tremag@rpi.edu>
2019-10-31 23:32:51 -0400

    figured out how to dynamically display the title of the current page
    I used php because it is more reliable (e.g. I don't have to rely on the client having JS enabled) than JS, and it comes with the added benefit of generating HTML before the browser ever has a chance of displaying it (no chance for a brief flash of placeholder text while the JS executes).
    I still need to clean up this file and figure out how to actually use the text I found.

--- resources/templates/header.php
+++ resources/templates/header.php
@@ -1,2 +1,22 @@
-<img id="logo" src="/resources/images/logo.png" width=50px height=50px alt="Logo">
-<h1 class="group_name"> Group Name </h1>
+<!-- Everything in this file is placed at the very top of the <body> tag in every page on the site -->
+    <img id="logo" src="/resources/images/logo.png" width=50px height=50px alt="Logo">
+    <?
+        $doc = new DomDocument();
+        // the following line *might be* necessary. dont ask me why. php is mean :(
+        //$doc->validateOnParse = true;
+        // turns out it's only necessary when I use the basic load method. hooray.
+
+        // the following line assigns the absolute path of the currently executing script to $file
+        $file = $_SERVER["SCRIPT_FILENAME"];
+        $bool = $doc->loadHTMLFile($file);
+        $titles = $doc->getElementsByTagname("title");
+        $out = array();
+        foreach($titles as $item) {
+            $out[] = $item->nodeValue;
+        }
+        // $out[0]; contains the text contained in the <title> tag in the html file where this file is include'd
+        // now I have to generate some html so that this text isn't just floating around on the page
+
+        echo $out[0];
+    ?>
+    <h1 class="group_name"> Group Name </h1>
