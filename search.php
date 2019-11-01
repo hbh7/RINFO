fcde522973c2a5e45dd583bf9768481a0c1f5356
ZhaoxiSun19991225 <47791231+ZhaoxiSun19991225@users.noreply.github.com>
2019-11-01 02:45:04 -0400

    Update search.php

--- search.php
+++ search.php
@@ -1,33 +1,15 @@
 <html>
     <head>
-        <?php include('resources/templates/head.php'); ?>
-        <title> RINFO Home </title>
+        <?php include('resources/templates/search_head.php'); ?>
+        <title> Searching... </title>
     </head>
     <body>
-        <?php include('resources/templates/header.php'); ?>
-
-        Welcome!
-
-        <div id="content">
-            <div id="information">
-                <h2>Information</h2>
-                <div id="information_content" class="content">
-                    <img id="group_logo" src="/resources/images/logo.png" alt="Group Logo">
-                    <h3 class="group_name">Group Name</h3>
-                    <p id="Nusers">X users</p>
-                    <p id="Nposts">X posts</p>
-                    <p id="join">Join</p>
-                </div>
-            </div>
-            <div id="activity">
-                <h2>Activities</h2>
-                <div id="activity_content" class="content">
-                    <ul>
-                        <li>....shirleyismygirly....</li>
-                        <li>...._______:)_______....</li>
-                    </ul>
-                </div>
-            </div>
+		<?php include('resources/templates/header.php'); ?>
+        <div calss="search-box">
+          <input calss="search-txt"type = "text" name="" placeholder="Type to search">
+		  <a class="search-btn">
+		  
+		  </a>
         </div>
 
         <?php include('resources/templates/footer.php'); ?>
