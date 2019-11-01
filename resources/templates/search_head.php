e75f662c71507913beee1e553f076d166d0c9f33
ZhaoxiSun19991225 <47791231+ZhaoxiSun19991225@users.noreply.github.com>
2019-11-01 03:21:12 -0400

    Update search_head.php

--- resources/templates/search_head.php
+++ resources/templates/search_head.php
@@ -3,3 +3,4 @@
 <link rel="stylesheet" type="text/css" href="/resources/styles/search_styles.css">
 <link rel="stylesheet" type="text/css" href="/resources/styles/styles-footer.css">
 <link rel="stylesheet" type="text/css" href="/resources/styles/styles-header.css">
+<script src="https://kit.fontawesome.com/11bb9a7ed5.js" crossorigin="anonymous"></script>
