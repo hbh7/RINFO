5427a89b1747600589f3ab03f08ebd190c240fef
Lanya Xiang <xianlany2@gmail.com>
2019-11-01 00:49:56 -0400

    Added fake data to group page
    Added fake data to RPISEC group page (HTML)

---
+++ group.php
@@ -0,0 +1,38 @@
+<html>
+    <head>
+        <?php include('resources/templates/head.php'); ?>
+        <title> RPISEC Home Page </title>
+    </head>
+    <body>
+        <?php include('resources/templates/header.php'); ?>
+        Welcome!
+
+        <div id="content">
+            <div id="information">
+                <h2>Information</h2>
+                <div id="information_content" class="content">
+                    <img id="group_logo" src="/resources/images/logo.png" alt="Group Logo">
+                    <h3 class="group_name">RPISEC</h3>
+                    <p id="Nusers">866 users</p>
+                    <p id="Nposts">70 posts</p>
+                    <p id="join">Join</p>
+                    <p>RPISEC is the resident computer security club at RPI. We are a student-run organization which was founded to provide resources for students interested in exploring the world of computer security.</p>
+                    <p><a href="https://rpis.ec/">Website Link</a></p>
+                    <p><a href="https://cs.sympa.rpi.edu/wws/subscribe/rpisec">Mailing List</a></p>
+                </div>
+            </div>
+            <div id="activity">
+                <h2>Activities</h2>
+                <div id="activity_content" class="content">
+                    <ul>
+                        <li>Club Meeting in CII 4050 on Friday Oct 18 from 5:00pm to 7:00pm</li>
+                        <li>Club Meeting next week rescheduled to Monday Oct 28 from 5-7</li>
+                        <li>CTF Competition from Tue, 22 Oct. 2019, 10:00 UTC — Thu, 24 Oct. 2019, 10:00 UTC</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <?php include('resources/templates/footer.php'); ?>
+    </body>
+</html>
