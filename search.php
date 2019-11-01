03400a89564e587e586634108cd102a4bd51b731
Gio <tremag@rpi.edu>
2019-11-01 07:06:30 -0400

    fixed typos in search.php and attempted to fix the footer
    possibly the reason why the search bar was being strange
    i did not succeed with the footer :(
    i have no idea how the search bar works, so I'd need to confer with whoever made it in order to figure out how to get the footer to actually be a footer for that page

--- search.php
+++ search.php
@@ -5,13 +5,15 @@
     </head>
     <body>
 		<?php include('resources/templates/header.php'); ?>
-        <div calss="search-box">
-          <input calss="search-txt"type = "text" name="" placeholder="Type to search">
-		  <a class="search-btn">
-		  	<i class="far fa-search"></i>
-		  </a>
-        </div>
+        <div id="search_box_and_footer_wrapper">
+            <div class="search-box">
+              <input class="search-txt" type="text" name="" placeholder="Type to search">
+    		  <a class="search-btn">
+    		  	<i class="far fa-search"></i>
+    		  </a>
+            </div>
 
-        <?php include('resources/templates/footer.php'); ?>
+            <?php include('resources/templates/footer.php'); ?>
+        </div>
     </body>
 </html>
