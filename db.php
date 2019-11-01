e9acb8f3b19d681c026b991f5621c83ab4b4c8a8
hbh7 <hbh7@hbh7.com>
2019-11-01 12:58:22 -0400

    Added db.php and edited newpost.php

--- db.php
+++ db.php
@@ -1,76 +1,70 @@
 <?php
-$servername = "localhost";
-$username = "rinfo";
-$password = "RINFOServerPassword10302019";
-$dbname = "RINFO";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+// Accepts a tablename and an array of values to insert
+function dbInsert($tablename, $dbdata) {
+    $conn = dbConnect();
 
-echo "Table r_users: <br />";
-$sql = "SELECT user_id, username, password, firstname, lastname, email FROM r_users";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "user_id: " . $row["user_id"] . ", username: " . $row["username"] . ", password: " . $row["password"] . ", firstname: " . $row["firstname"] . ", lastname: " . $row["lastname"] . ", email: " . $row["email"] . "<br>";
+    $dbdata_string = implode(", ", $dbdata);
+
+    if($tablename == "r_users") {
+        $sql = "INSERT INTO r_users (username, password, firstname, lastname, email) VALUES (" . $dbdata_string . ")";
+    } elseif ($tablename == "r_groups") {
+        $sql = "INSERT INTO r_groups (name, tagline) VALUES ("  . $dbdata_string . ")";
+    } elseif ($tablename == "r_permissions") {
+        $sql = "INSERT INTO r_permissions (user_id, group_id, description) VALUES ("  . $dbdata_string . ")";
+    } elseif ($tablename == "r_subscriptions") {
+        $sql = "INSERT INTO r_subscriptions (user_id, group_id) VALUES ("  . $dbdata_string . ")";
+    } elseif ($tablename == "r_posts") {
+        $sql = "INSERT INTO r_posts (group_id, user_id, title, body, date) VALUES ("  . $dbdata_string . ")";
     }
-} else {
-    echo "0 results <br />";
-}
 
-echo "<br />Table r_groups: <br />";
-$sql = "SELECT group_id, name, tagline, logo FROM r_groups";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "group_id: " . $row["group_id"] . ", name: " . $row["name"] . ", tagline: " . $row["tagline"] . ", logo: " . $row["logo"] . "<br>";
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
     }
-} else {
-    echo "0 results <br />";
+
+    $conn->close();
 }
 
-echo "<br />Table permissions: <br />";
-$sql = "SELECT permission_id, user_id, group_id, description FROM r_permissions";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "permission_id: " . $row["permission_id"] . ", user_id: " . $row["user_id"] . ", group_id: " . $row["group_id"] . ", description: " . $row["description"] . "<br>";
+// Accepts a tablename and an array of values to insert
+function dbGet($tablename, $dbdata) {
+    $conn = dbConnect();
+
+    $dbdata_string = implode(", ", $dbdata);
+
+    echo "Table " . $tablename . ": <br />";
+    $sql = "SELECT " . $dbdata_string . " FROM " . $tablename;
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo $row . "<br />"; // or something else, undecided yet how this is going to get used.
+        }
+    } else {
+        echo "0 results <br />";
     }
-} else {
-    echo "0 results <br />";
+
+    $conn->close();
 }
 
-echo "<br />Table r_subscriptions: <br />";
-$sql = "SELECT subscription_id, user_id, group_id FROM r_subscriptions";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "subscription_id: " . $row["subscription_id"] . ", user_id: " . $row["user_id"] . ", group_id: " . $row["group_id"] . "<br>";
+function dbConnect() {
+    $servername = "localhost";
+    $username = "rinfo";
+    $password = "RINFOServerPassword10302019";
+    $dbname = "RINFO";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
     }
-} else {
-    echo "0 results <br />";
-}
 
-echo "<br />Table r_posts: <br />";
-$sql = "SELECT post_id, group_id, user_id, title, body, date FROM r_posts";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "post_id: " . $row["post_id"] . ", group_id: " . $row["group_id"] . ", user_id: " . $row["user_id"] . ", title: " . $row["title"] . ", body: " . $row["body"] . ", date: " . $row["date"] . "<br>";
-    }
-} else {
-    echo "0 results <br />";
+    return $conn;
 }
 
 
-$conn->close();
+
+
 ?>
