9538ee5669ad2ab684310777f37e2b93b80bb5b6
hbh7 <hbh7@hbh7.com>
2019-11-17 00:58:20 -0500

    Slightly prettier footer

--- resources/templates/footer.php
+++ resources/templates/footer.php
@@ -2,6 +2,6 @@
     <footer id="global_footer">
         <span> Made by Team Tokabo </span>
         <span> Web Systems and Development Semester Project </span>
-        <span><a href="#"> Go To Site Map </a></span>
+        <span><a href="#"> Site Map </a></span>
     </footer>
 </div>
