c5a9737805bed704d53531c8a5073f9a15608cbc
hbh7 <hbh7@hbh7.com>
2019-11-24 20:59:18 -0500

    New Group page PHP created, can now create groups

--- newgroup.php
+++ newgroup.php
@@ -1,3 +1,43 @@
+<?php
+
+// Get POST data
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    include_once 'db.php';
+
+    if(checkValidLogin()) {
+
+        // Check if any groups exist with the same name
+        $arr = dbGet("name", "r_groups");
+        $unique = true;
+        foreach($arr as $a) {
+            if($a["name"] == $_POST["name"]) {
+                $unique = false;
+                break;
+            }
+        }
+        if($unique) {
+
+            dbPut("r_groups", [$_POST["name"], $_POST["name"]]);
+            $groupID = dbGet("*", "r_groups", "name='" . $_POST["name"] . "'")[0]["group_id"];
+
+            header("Location: /group.php?group_id=" . $groupID);
+            die();
+
+        } else {
+            // Throw an error that the group name isn't unique. TODO: Improve this
+            echo "Error, your group name isn't unique";
+        }
+
+    } else {
+        header("Location: /login.php?redirectmsg=You must be logged in to do that!");
+        die();
+    }
+
+
+}
+?>
+
+
 <html lang="en">
     <head>
         <?php include('resources/templates/head.php'); ?>
@@ -8,28 +48,21 @@
         <?php include('resources/templates/header.php'); ?>
         <?php include_once('db.php'); ?>
 
-        <form>
-            Event Name:
-            <input type="text" name="eventName"><br>
-            Date:
-            <input type="date" name="date"><br>
-            Start Time:
-            <input type="time" name="startTime"><br>
-            End Time:
-            <input type="time" name="endTime"><br>
-            Location:
-            <input type="text" name="location"><br>
-            Room Number:
-            <input type="text" name="roomNum"><br>
-            Description:
-            <input type="text" name="description"><br>
+        <form method="post" action="newgroup.php">
+            <!-- TODO: These need label tags -->
+            <!-- TODO: Validate form data -->
+            Group Name:
+            <input type="text" name="name" value="<?php if (isset($_POST['name'])) echo $_POST['name']; ?>"><br>
+            Tagline:
+            <input type="text" name="tagline" value="<?php if (isset($_POST['tagline'])) echo $_POST['tagline']; ?>"><br>
+
+
+            <label>Example: <input type="text" name="example"></label><br />
+
+            <input type="submit" />
+
         </form>
 
-        <?php
-
-        // Idk, when the button is pushed, call the insertDB(); function or something
-
-        ?>
 
         <?php include('resources/templates/footer.php'); ?>
     </body>
