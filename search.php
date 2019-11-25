f854a639e3e09792fb563ed7afc180031a23f2bc
hbh7 <hbh7@hbh7.com>
2019-11-25 13:25:30 -0500

    Disabled some search page fanciness because it was annoying

--- search.php
+++ search.php
@@ -1,13 +1,14 @@
 <html lang="en">
     <head>
-        <?php include('resources/templates/search_head.php'); ?>
+        <?php //include('resources/templates/search_head.php'); ?>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <title> Searching... </title>
     </head>
     <body>
 		<?php include('resources/templates/header.php'); ?>
         <div id="search_box_and_footer_wrapper">
             <div class="search-box">
-              <input class="search-txt" type="text" name="" placeholder="Type to search">
+              <input class="search-txt" type="text" name="search" id="searchtextbox" placeholder="Type to search">
     		  <a class="search-btn">
     		  	<i class="far fa-search"></i>
     		  </a>
@@ -15,5 +16,7 @@
 
             <?php include('resources/templates/footer.php'); ?>
         </div>
+
+        <script type="text/javascript" src="resources/scripts/search.js"></script>
     </body>
 </html>
