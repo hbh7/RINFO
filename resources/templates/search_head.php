69ea36aebeff064c27d7195a5e1dacae12718f20
Gio <tremag@rpi.edu>
2019-11-28 00:00:18 -0800

    included bootstrap in search_head
    the fonts on the search page were different from the rest of the site
    i realized that it was because bootstrap was not being used, so i included it (seems to have fixed the problem)

--- resources/templates/search_head.php
+++ resources/templates/search_head.php
@@ -1,5 +1,9 @@
 <!-- Load scripts, styles, etc for search page here -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
 <link rel="stylesheet" type="text/css" href="/resources/styles/search_styles.css">
 <link rel="stylesheet" type="text/css" href="/resources/styles/styles-footer.css">
 <link rel="stylesheet" type="text/css" href="/resources/styles/styles-header.css">
