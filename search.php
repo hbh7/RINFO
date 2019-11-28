add22699791e791f21c4203367d400aa94f5b25f
ZhaoxiSun19991225 <47791231+ZhaoxiSun19991225@users.noreply.github.com>
2019-11-27 22:50:09 -0500

    uncomment the search page

--- search.php
+++ search.php
@@ -1,6 +1,6 @@
 <html lang="en">
     <head>
-        <?php //include('resources/templates/search_head.php'); ?>
+        <?php include('resources/templates/search_head.php'); ?>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <title> Searching... </title>
     </head>
