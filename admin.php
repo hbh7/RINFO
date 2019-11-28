b20182273e240b646244608cdc753c6950c284db
Gio <tremag@rpi.edu>
2019-11-28 03:13:40 -0800

    i removed the outer php tags because i changed my mind
    so here's my idea for the admin page:
    left column has a bunch of actions that a particular admin can do (there were 2 types of admins in the project specification), and the right column has whatever gui is necessary for the currently selected action to be completed
    that way i only have to make one, very long admin php file (with maybe a few helper files idk yet)

--- admin.php
+++ admin.php
@@ -5,35 +5,30 @@
     </head>
     <body>
         <?php include('resources/templates/header.php'); ?>
-        Welcome!
-    <?php
-        echo <<<HTML
-            <div id="content">
-                <div id="information">
-                    <h2>Information</h2>
-                    <div id="information_content" class="content">
-                        <img id="group_logo" src="/resources/images/logo.png" alt="Group Logo">
-                        <h3 class="group_name">RPI Admins</h3>
-                        <p id="Nusers">13 users</p>
-                        <p id="Nposts">12 posts</p>
-                        <p id="Join">Edit Front Page Banner</p>
-                        <p>This is the official group for the RPI administration.</p>
-                        <p><a href="https://rpi.edu/">Website Link</a></p>
-                        <p><a href="https://lists.sympa.rpi.edu/wws/info/morningmail">Mailing List</a></p>
-                    </div>
-                </div>
-                <div id="activity">
-                    <h2>Notifications</h2>
-                    <div id="activity_content" class="content">
-                        <ul>
-                            <li>There will be no classes on Jan. 17th due to a snow day! Nah jk this is RPI. You thought we would show mercy? By executive decree, today only, all 8am classes shall start at 8:06am.</li>
-                            <li>RPI ALERT: A student on 15th street was approached by a 2'4" man wearing an Elmo costume. Witnesses say, "It may have been an infant on Halloween"</li>
-                        </ul>
-                    </div>
+        <div id="content">
+            <div id="information">
+                <h2>Actions</h2>
+                <div id="information_content" class="content">
+                    <img id="group_logo" src="/resources/images/logo.png" alt="Group Logo">
+                    <h3 class="group_name">RPI Admins</h3>
+                    <p id="Nusers">13 users</p>
+                    <p id="Nposts">12 posts</p>
+                    <p id="Join">Edit Front Page Banner</p>
+                    <p>This is the official group for the RPI administration.</p>
+                    <p><a href="https://rpi.edu/">Website Link</a></p>
+                    <p><a href="https://lists.sympa.rpi.edu/wws/info/morningmail">Mailing List</a></p>
                 </div>
             </div>
-HTML;
-    ?>
+            <div id="activity">
+                <h2>Notifications</h2>
+                <div id="activity_content" class="content">
+                    <ul>
+                        <li>There will be no classes on Jan. 17th due to a snow day! Nah jk this is RPI. You thought we would show mercy? By executive decree, today only, all 8am classes shall start at 8:06am.</li>
+                        <li>RPI ALERT: A student on 15th street was approached by a 2'4" man wearing an Elmo costume. Witnesses say, "It may have been an infant on Halloween"</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
         <?php include('resources/templates/footer.php'); ?>
     </body>
 </html>
