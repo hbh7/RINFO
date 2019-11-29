8f666c951583d04a62dac227b85a420fe2ecae70
Gio <tremag@rpi.edu>
2019-11-28 16:01:25 -0800

    removed a problematic class from the footer, made it responsive
    footer now shows up and is formatted nicely as long as the width is above 379px

--- resources/templates/footer.php
+++ resources/templates/footer.php
@@ -1,5 +1,5 @@
 <nav id="footer_nav" class="navbar fixed-bottom navbar-expand-sm navbar-dark bg-dark">
-    <div class="collapse navbar-collapse" id="wrap_footer">
+    <div class="navbar-collapse" id="wrap_footer">
         <footer id="global_footer">
             <span> Made by Team Tokabo </span>
             <span> Web Systems and Development Semester Project </span>
