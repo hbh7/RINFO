07208814b600a60255e1794ec814213ebe7610ba
Lanya Xiang <xianlany2@gmail.com>
2019-11-29 15:40:14 -0500

    started work on groups page
    changed the posts box to a timeline style item, did some reorganization of the style files, moved some stuff from the general file to page specific file

--- resources/templates/head.php
+++ resources/templates/head.php
@@ -2,7 +2,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 
 <link rel="stylesheet" type="text/css" href="/resources/styles/styles-login.css">
 <link rel="stylesheet" type="text/css" href="/resources/styles/search_styles.css">
@@ -11,4 +11,3 @@
 <link rel="stylesheet" type="text/css" href="/resources/styles/styles-footer.css">
 <link rel="stylesheet" type="text/css" href="/resources/styles/styles-header.css">
 <link rel="stylesheet" type="text/css" href="/resources/styles/styles.css">
-
