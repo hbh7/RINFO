7da4039d6fd31d9a9b834fe3447b9b8ed5ba49a6
Lanya Xiang <xianlany2@gmail.com>
2019-12-01 18:16:42 -0500

    updated link colors on search page

--- resources/templates/footer.php
+++ resources/templates/footer.php
@@ -3,7 +3,7 @@
         <footer id="global_footer">
             <span> Made by Team Tokabo </span>
             <span> Web Systems and Development Semester Project </span>
-            <span><a href="#"> Site Map </a></span>
+            <span><a id="site_map" href="#"> Site Map </a></span>
         </footer>
     </div>
 </nav>
