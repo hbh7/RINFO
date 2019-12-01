d02e203bf8a1dfabba48e1c651b6728c870007ee
Lanya Xiang <xianlany2@gmail.com>
2019-12-01 15:57:07 -0500

    user page now displaying groups and posts

--- user.php
+++ user.php
@@ -52,37 +52,37 @@ $numPosts = sizeof(dbGet("post_id", "r_posts", "user_id='" . $user_id . "' AND g
                 <h3 id="name"><?php echo $user["firstname"] . " " . $user["lastname"]; ?></h3>
                 <h3 id="username"><?php echo $user["username"]; ?></h3>
                 <h4 id="email"><?php echo $user["email"]; ?></h3>
-                <!-- <p id="Nusers"><?php echo $numSubscriptions; ?> users</p>
+                    <!-- <p id="Nusers"><?php echo $numSubscriptions; ?> users</p>
                 <p id="Nposts"><?php echo $numPosts; ?> posts</p> -->
-                <form method="post">
-                    <?php
-                    if (getUserID() != $user_id) {
-                        if (checkValidLogin()) {
-                            if (sizeof(dbGet("subscription_id", "r_subscriptions", "user_id='" . $user_id . "' AND user_id='" . getUserID() . "'")) > 0) {
-                                echo "<button type=\"submit\" id=\"join\" name=\"action\" value=\"leave\">Unfollow</button>";
+                    <form method="post">
+                        <?php
+                        if (getUserID() != $user_id) {
+                            if (checkValidLogin()) {
+                                if (sizeof(dbGet("subscription_id", "r_subscriptions", "user_id='" . $user_id . "' AND user_id='" . getUserID() . "'")) > 0) {
+                                    echo "<button type=\"submit\" id=\"join\" name=\"action\" value=\"leave\">Unfollow</button>";
+                                } else {
+                                    echo "<button type=\"submit\" id=\"join\" name=\"action\" value=\"join\">Follow</button>";
+                                }
                             } else {
                                 echo "<button type=\"submit\" id=\"join\" name=\"action\" value=\"join\">Follow</button>";
                             }
-                        } else {
-                            echo "<button type=\"submit\" id=\"join\" name=\"action\" value=\"join\">Follow</button>";
                         }
-                    }
-                    ?>
-                </form>
-                <div class="row">
-                    <div class="col-12">
-                        <div class="list-group" id="list-tab" role="tablist">
-                            <a class="list-group-item list-group-item-action active" id="list-notifications-list" data-toggle="list" href="#list-notifications" role="tab" aria-controls="notifications">My Groups</a>
-                            <a class="list-group-item list-group-item-action" id="list-your_groups-list" data-toggle="list" href="#list-your_groups" role="tab" aria-controls="your_groups">My Posts</a>
+                        ?>
+                    </form>
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="list-group" id="list-tab" role="tablist">
+                                <a class="list-group-item list-group-item-action active" id="list-notifications-list" data-toggle="list" href="#list-notifications" role="tab" aria-controls="notifications">My Groups</a>
+                                <a class="list-group-item list-group-item-action" id="list-your_groups-list" data-toggle="list" href="#list-your_groups" role="tab" aria-controls="your_groups">My Posts</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <!-- <p><a href="https://rpis.ec/">Website Link</a></p>
+                    <!-- <p><a href="https://rpis.ec/">Website Link</a></p>
                     <p><a href="https://cs.sympa.rpi.edu/wws/subscribe/rpisec">Mailing List</a></p>
                     TODO: Move this into the database, and make it so it can be changed online-->
             </div>
         </div>
-        <div id="activity">
+        <!-- <div id="activity">
             <h2>Posts</h2>
             <div id="activity_content" class="content">
                 <?php
@@ -99,7 +99,7 @@ $numPosts = sizeof(dbGet("post_id", "r_posts", "user_id='" . $user_id . "' AND g
                 }
                 ?>
             </div>
-        </div>
+        </div> -->
         <div id="activity">
             <div id="activity_content" class="col-17">
                 <div class="tab-content" id="nav-tabContent">
@@ -107,16 +107,49 @@ $numPosts = sizeof(dbGet("post_id", "r_posts", "user_id='" . $user_id . "' AND g
                         <h2>My Groups</h2>
                         <ul class="tab-content-ul">
                             <!--TODO: Someone do me a favor and generate this from the database-->
-                            <li>There will be no classes on Jan. 17th due to a snow day! Nah jk this is RPI. You thought we would show mercy? By executive decree, today only, all 8am classes shall start at 8:06am.</li>
-                            <li>RPI ALERT: A student on 15th street was approached by a 2'4" man wearing an Elmo costume. Witnesses say, "It may have been an infant on Halloween"</li>
+                            <?php
+
+                            if (checkValidLogin()) {
+                                $subscriptions = dbGet("group_id", "r_subscriptions", "user_id='" . getUserID() . "'");
+                                foreach ($subscriptions as $subscription) {
+                                    $name = dbGet("name", "r_groups", "group_id=" . $subscription["group_id"])[0]["name"];
+
+                                    echo "<div class='group'>" .
+                                        "<span class='name'><a href=\"group.php?group_id=" . $subscription["group_id"] . "\">" . $name . "</a></span><br />" .
+                                        "</div>";
+                                }
+                                //is there a way to display a "No groups joined" message?
+                            }
+
+                            // echo "<span> All groups </span>";
+
+                            // $groups = dbGet("group_id, name", "r_groups");
+                            // foreach ($groups as $group) {
+                            //     echo "<div class='group'>" .
+                            //         "<span class='name'><a href=\"group.php?group_id=" . $group["group_id"] . "\">" . $group["name"] . "</a></span><br />" .
+                            //         "</div>";
+                            // }
+
+
+                            ?>
                         </ul>
                     </div>
                     <div class="tab-pane fade" id="list-your_groups" role="tabpanel" aria-labelledby="list-your_groups-list">
                         <h2>My Posts</h2>
                         <ul class="tab-content-ul">
-                            <!--TODO: Someone do me a favor and generate this from the database-->
-                            <li>There will be no classes on Jan. 17th due to a snow day! Nah jk this is RPI. You thought we would show mercy? By executive decree, today only, all 8am classes shall start at 8:06am.</li>
-                            <li>RPI ALERT: A student on 15th street was approached by a 2'4" man wearing an Elmo costume. Witnesses say, "It may have been an infant on Halloween"</li>
+                            <?php
+                            $posts = dbGet("*", "r_posts", "user_id='" . $user_id . "' AND group_id='0'");
+                            foreach ($posts as $post) {
+                                $name = dbGet("firstname, lastname", "r_users", "user_id=" . $post["user_id"]);
+
+                                echo "<li class='my_post'>" .
+                                    "<span class='title'>" . $post["title"] . "</span><br />" .
+                                    "<span class='body'>" . $post["body"] . "</span><br />" .
+                                    "<span class='postauthor'> Posted by " . $name[0]["firstname"] . " " . $name[0]["lastname"] . "</span>" .
+                                    "<span class='postdate'> on " . $post["timestamp"] . "</span>" .
+                                    "</li>";
+                            }
+                            ?>
                         </ul>
                     </div>
                 </div>
