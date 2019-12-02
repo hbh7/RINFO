1a12a3f1c3a803bc6839670f0b5b0f13d85669cc
Ethan Goedken <evgoedken@gmail.com>
2019-12-02 16:59:10 -0500

    Comment Textarea placeholders

--- resources/scripts/comments.js
+++ resources/scripts/comments.js
@@ -6,13 +6,3 @@ $(".replybutton").click(function() {
 		$(this).parent().find(".reply_box:first").slideToggle("slow");	
 	}
 });
-
-var last_text = "";
-$("textarea[name='comment_body'").focusin(function() {
-	last_text = $(this).html();
-	$(this).html("");
-});
-$("textarea[name='comment_body'").focusout(function() {
-	$(this).html(last_text);
-	last_text = "";
-});
