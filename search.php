bfdaa73d666082816c4a67b7408f8b8f642adc6d
hbh7 <hbh7@hbh7.com>
2019-12-02 00:57:51 -0500

    Revert "Search should not have its own head file"
    This reverts commit 98041bf128665b0ee3dc8d04b38778d70e1f28d8.

--- search.php
+++ search.php
@@ -2,7 +2,7 @@
 
 <head>
     <?php $title = "Search"; ?>
-    <?php include('resources/templates/head.php'); ?>
+    <?php include('resources/templates/search_head.php'); ?>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </head>
 
