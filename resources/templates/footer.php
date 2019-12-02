1f71a3e7238b5019c8548958a4b5c0193d53a706
Lanya Xiang <xianlany2@gmail.com>
2019-12-02 16:09:05 -0500

    changed color of footer to match background

--- resources/templates/footer.php
+++ resources/templates/footer.php
@@ -1,4 +1,4 @@
-<nav id="footer_nav" class="navbar fixed-bottom navbar-expand-sm navbar-dark bg-dark">
+<nav id="footer_nav" class="navbar fixed-bottom navbar-expand-sm navbar-dark">
     <div class="navbar-collapse" id="wrap_footer">
         <footer id="global_footer">
             <span> Made by Team Tokabo </span>
