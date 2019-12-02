d410dfc7a4d790a615c206fb800be1e0c21f4c2a
ZhaoxiSun19991225 <47791231+ZhaoxiSun19991225@users.noreply.github.com>
2019-12-02 16:02:31 -0500

    clean up the search page and add some comment

--- search.php
+++ search.php
@@ -1,3 +1,4 @@
+<!--Search Page-->
 <html lang="en">
 
 <head>
@@ -7,44 +8,23 @@
 </head>
 
 <body>
+    <!--Add the header of the search page. ex: Logo, title, and buttons-->
     <?php include('resources/templates/header.php'); ?>
-    <!--<div id="search_box_and_footer_wrapper">
-        <div class="search-box">
-            <input class="search-txt" type="text" name="search" id="searchtextbox" placeholder="Type to search" autocomplete="off">
-            <img src="https://img.icons8.com/nolan/64/000000/search.png" id="search-icon">
-        </div>
-        <div id="results"></div>-->
     <div id="search_box_and_footer_wrapper">
-        <!--https://codepen.io/AlbertFeynman/pen/BPvzWZ-->
         <div class="search-box">
             <div class="container">
                 <input type="text" placeholder="Type to Search..." class="search-txt" name="search" id="searchtextbox" autocomplete="off">
                 <div class="search"></div>
             </div>
         </div>
+        <!--result shows in this div-->
         <div id="results"></div>
-
+        <!--Add the footer for the search page-->
         <?php include('resources/templates/footer.php'); ?>
     </div>
-
+    <!--Put the javascript at the bottom to avid it slow down the whole page.-->
     <script type="text/javascript" src="resources/scripts/search.js"></script>
 </body>
 
 </html>
 
-
-<!--<div id="search_box_and_footer_wrapper">
-    <div class="search-box">
-        <div class="container">
-            <input type="text" placeholder="Type to Search..." class="search-txt" name="search" id="searchtextbox" autocomplete="off">
-            <div class="search"></div>
-        </div>
-        <input class="search-txt" type="text" name="search" id="searchtextbox" placeholder="Type to search" autocomplete="off">
-            <div id="animation-icon">
-                <div id="icon" class="play">
-                    <img src="https://img.icons8.com/nolan/64/000000/search.png" class="search-icon">
-                    <img src="https://img.icons8.com/ultraviolet/40/000000/go.png" class="search-icon">
-                </div>
-            </div>
-    </div>
-    <div id="results"></div>-->
