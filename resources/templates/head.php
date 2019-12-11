0aaf86550cfb876c5c65a0aac05be994f844405e
hbh7 <hbh7@hbh7.com>
2019-12-10 23:32:32 -0500

    Clean up head.php

--- resources/templates/head.php
+++ resources/templates/head.php
@@ -2,24 +2,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-
-<!--
-TODO: Fix the dupe rel above
-<link rel="stylesheet" type="text/css" href="/resources/styles/styles-newpost.css">
-<link rel="stylesheet" type="text/css" href="/resources/styles/styles-newgroup.css">
-<link rel="stylesheet" type="text/css" href="/resources/styles/styles.css">
-<link rel="stylesheet" type="text/css" href="/resources/styles/styles-login.css">
-<link rel="stylesheet" type="text/css" href="/resources/styles/search_styles.css">
-<link rel="stylesheet" type="text/css" href="/resources/styles/styles-group.css">
-<link rel="stylesheet" type="text/css" href="/resources/styles/styles-homepage.css">
-<link rel="stylesheet" type="text/css" href="/resources/styles/styles-footer.css">
-<link rel="stylesheet" type="text/css" href="/resources/styles/styles-header.css">
-TODO: Delete these
--->
-
-
-
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" id="bootstrap-css">
 
 <?php
     // of course I can't just read in a url and do stuff with it
