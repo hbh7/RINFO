298563d4e6a01d94b05100ed1dde3785ba41ee11
Lanya Xiang <xianlany2@gmail.com>
2019-12-11 20:21:47 -0500

    added link to group name

--- resources/templates/footer.php
+++ resources/templates/footer.php
@@ -1,9 +1,9 @@
 <nav id="footer_nav" class="navbar fixed-bottom navbar-expand-sm navbar-dark">
     <div class="navbar-collapse" id="wrap_footer">
         <footer id="global_footer">
-            <span> Made by Team Tokabo </span>
+            <span><a id="tokabo" href="https://www.ikea.com/ca/en/p/tokabo-table-lamp-glass-purple-40405904/">Made by Team Tokabo</a> </span>
             <span> Web Systems and Development Semester Project </span>
-            <span><a id="site_map" href="#"> Site Map </a></span>
+            <!-- <span><a id="site_map" href="#"> Site Map </a></span> -->
             <!-- TODO: Site map link does not work -->
         </footer>
     </div>
